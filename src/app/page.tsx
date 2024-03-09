efe70c48bd83ad0e3d9d3e2956cb5778cfd7f3c1
JEAN GAL <jgatbkk@gmail.com>
2024-03-09 17:11:22 -0500

    main page fill up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import { Flex, Heading } from "@chakra-ui/react";
 export default function Home() {
   return (
     <main>
-      <Flex>
+      <Flex
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        width="100%">
         <Heading>Story Protocol</Heading>
-
       </Flex>
     </main>
   );
