d86de6061a2543ff99e904d69261b7056e2004b0
JEAN GAL <jgatbkk@gmail.com>
2024-03-10 19:57:14 -0400

    mintNFT component

---
+++ src/app/components/mintNft.tsx
@@ -0,0 +1,34 @@
+import { Address } from "viem";
+import { publicClient, walletClient } from "../contexts/useStoryClient";
+import abi from "../../contracts/swe.abi";
+
+export async function mintNFT(): Promise<string> {
+    if (!walletClient) {
+        throw new Error("No wallet client found");
+    }
+
+    const [account] = await walletClient.getAddresses();
+
+    const { result } = await publicClient.simulateContract({
+        address: process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS as Address,
+        functionName: 'mint',
+        account,
+        args: ["https://www.taketheredbean.com/"],
+        abi: abi
+    })
+
+    const hash = await walletClient.writeContract({
+        address: process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS as Address,
+        functionName: 'mint',
+        account: account,
+        args: ["https://www.taketheredbean.com/"],
+        abi: abi
+    });
+
+    let tokenId = result!.toString();
+
+    console.log(`Minted NFT successful with hash: ${hash}`);
+    console.log(`Minted NFT tokenId: ${tokenId}`);
+
+    return tokenId;
+}
