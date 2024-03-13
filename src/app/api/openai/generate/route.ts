8ec25eab76fa1961be781b95ebd8868cdd862cf0
JEAN GAL <jgatbkk@gmail.com>
2024-03-13 00:39:52 -0400

    imported open ai api for image generation based on prompt

---
+++ src/app/api/openai/generate/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY });
+
+export async function POST(req: NextRequest) {
+
+    const { prompt } = await req.json();
+
+    const response = await openai.images.generate({
+        response_format: 'b64_json', // or 'url'
+        prompt: prompt || "story protocol",
+        model: "dall-e-2",
+        quality: 'standard', // or 'hd' -> only for dall-e-3
+        size: '1024x1024', //'1024x1024' | '1792x1024' | '1024x1792' -> for dall-e-3
+        style: 'natural', // or natural
+        n: 1, // number of images to generate
+    });
+
+    console.log(response.data);
+    const image_b64 = response.data[0].b64_json;
+    return NextResponse.json({ image_b64 }, { status: 200 });
+}
--- src/app/components/MintPage.tsx
+++ src/app/components/MintPage.tsx
@@ -1,18 +1,19 @@
 'use client';
 
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, Flex, Input } from "@chakra-ui/react";
 import { mint } from "./mint";
 import { registerRootIp } from "./registerRoot";
 import { StoryClient } from "@story-protocol/core-sdk";
 import { PublicClient, WalletClient } from "viem";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ClientsContext } from "../providers/clientsProvider";
 
 export default function MintPage() {
-
     const walletClient: WalletClient | undefined = useContext(ClientsContext)?.walletClient;
     const publicClient: PublicClient | undefined = useContext(ClientsContext)?.publicClient;
     const storyClient: StoryClient | undefined = useContext(ClientsContext)?.storyClient;
+
+    const [prompt, setPrompt] = useState('');
 
     const handleMint = async () => {
         const result = await mint(publicClient!, walletClient!);
@@ -24,12 +25,34 @@ export default function MintPage() {
         console.log("the result is ", result);
     }
 
+    const handleImageGeneration = async () => {
+        const response = await fetch("/api/openai/generate", {
+            method: "POST",
+            body: JSON.stringify({ prompt }),
+        });
+
+        if (!response.ok) {
+            console.error('API call failed:', response);
+            return;
+        }
+
+        const { image_b64 } = await response.json();
+        console.log("the result is ", image_b64);
+    }
+
     return (
         <Flex
             flexDirection="column"
             justifyContent="center"
             alignItems="center"
             width="100%">
+            <Input
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                placeholder="Write a prompt for AI generation"
+                mb={4}
+            />
+            <Button onClick={handleImageGeneration}>Generate Image</Button>
             <Button onClick={handleMint}>Mint Nft</Button>
             <Button onClick={handleRegisterRoot}>Register Root IP</Button>
         </Flex>
