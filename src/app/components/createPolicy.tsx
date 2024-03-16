c88f902336789c15fcd70053ea3057bc691017b0
JEAN GAL <jgatbkk@gmail.com>
2024-03-16 13:42:14 -0400

    update createPolicyComponent

--- src/app/components/createPolicy.tsx
+++ src/app/components/createPolicy.tsx
@@ -13,14 +13,10 @@ export default function CreatePolicy({ setPolicyId }: { setPolicyId: React.Dispa
     const [derivativesAllowed, setDerivativesAllowed] = useState(true);
     const [derivativesApproval, setDerivativesApproval] = useState(true);
     const [derivativesReciprocal, setDerivativesReciprocal] = useState(true);
-    const [commercialRevShare, setCommercialRevShare] = useState<number | undefined>();
-    const [territories, setTerritories] = useState<string[] | undefined>();
-    const [distributionChannels, setDistributionChannels] = useState<string[] | undefined>();
 
     const storyClient: StoryClient | undefined = useContext(ClientsContext)?.storyClient;
 
     const handleCreatePolicy = async () => {
-
         if (!storyClient) {
             console.log("Story client not found. Please check if the client is initialized.");
             return;
@@ -34,10 +30,7 @@ export default function CreatePolicy({ setPolicyId }: { setPolicyId: React.Dispa
             derivativesAttribution: derivativesAttribution,
             derivativesAllowed: derivativesAllowed,
             derivativesApproval: derivativesApproval,
-            derivativesReciprocal: derivativesReciprocal,
-            territories: territories,
-            distributionChannels: distributionChannels,
-            commercialRevShare: commercialRevShare
+            derivativesReciprocal: derivativesReciprocal
         }
 
         const response = await storyClient.policy.registerPILPolicy({
@@ -50,8 +43,8 @@ export default function CreatePolicy({ setPolicyId }: { setPolicyId: React.Dispa
     };
 
     return (
-        <Flex flexDirection="column" mr={4} textColor="white" borderColor="gray.800" borderWidth={1} p={4} borderRadius="xl">
-            <Text mb={4} textColor="gray.800" fontSize="large">4 - Choose PIL Policy Parameters</Text>
+        <Flex flexDirection="column" textColor="white" borderColor="gray.800" borderWidth={1} p={4} borderRadius="xl" w="100%">
+            <Text mb={4} textColor="gray.800" fontSize="large">3 - Choose PIL Policy Parameters</Text>
             <Grid as="form" backgroundColor="gray.700" templateColumns="repeat(3, 1fr)" borderRadius="lg" rowGap={0} p={4} h="100%" w="100%" columnGap={8} >
                 <FormControl display="flex" alignItems="center" mb={4}>
                     <FormLabel mb="0" fontSize="small">Transferable:</FormLabel>
@@ -85,23 +78,12 @@ export default function CreatePolicy({ setPolicyId }: { setPolicyId: React.Dispa
                     <FormLabel mb="0" fontSize="small">Derivatives Reciprocal:</FormLabel>
                     <Switch size="sm" isChecked={derivativesReciprocal} onChange={(e) => setDerivativesReciprocal(e.target.checked)} />
                 </FormControl>
-                <FormControl display="flex" alignItems="center" mb={4}>
-                    <FormLabel mb="0" fontSize="small">Commercial Rev Share:</FormLabel>
-                    <NumberInput value={commercialRevShare || ''} onChange={(valueString) => setCommercialRevShare(Number(valueString))} />
-                </FormControl>
-                <FormControl display="flex" alignItems="center">
-                    <FormLabel mb="0" fontSize="small">Territories:</FormLabel>
-                    <Textarea size="sm" value={territories?.join(', ') || ''} onChange={(e) => setTerritories(e.target.value.split(', '))} h="10px" />
-                </FormControl>
-                <FormControl display="flex" alignItems="center">
-                    <FormLabel mb="0" fontSize="small">Distribution Channels:</FormLabel>
-                    <Textarea size="sm" fontSize="sm" value={distributionChannels?.join(', ') || ''} onChange={(e) => setDistributionChannels(e.target.value.split(', '))} />
-                </FormControl>
-                <Flex justifyContent="left" alignItems="center">
-                    <Button textColor="gray.700" backgroundColor="white" fontSize="sm" size="sm" w="50%" onClick={handleCreatePolicy} ml={4}>
-                        Create Policy
-                    </Button>
-                </Flex>
+                <Button
+                    size="sm"
+                    onClick={handleCreatePolicy}
+                >
+                    Create Policy
+                </Button>
             </Grid>
         </Flex >
     );
