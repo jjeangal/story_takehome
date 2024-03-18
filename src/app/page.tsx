c2da9488e47244293ed862d3fe99b5d28062212a
JEAN GAL <jgatbkk@gmail.com>
2024-03-17 22:10:02 -0400

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
-
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading, List, ListItem } from "@chakra-ui/react";
 
 export default function Home() {
 
@@ -9,10 +8,27 @@ export default function Home() {
       flexDirection="column"
       justifyContent="center"
       alignItems="center"
+      textColor="black"
       w="100%"
       h="100%"
     >
-
+      <Heading mb={4}>
+        Welcome!
+      </Heading>
+      <List spacing={3} textAlign="center">
+        <ListItem>
+          You can:
+        </ListItem>
+        <ListItem>
+          Generate an image and register it as an IPAsset
+        </ListItem>
+        <ListItem>
+          Or
+        </ListItem>
+        <ListItem>
+          Explore IPassets, create variations of the images and register them as derivative IP assets
+        </ListItem>
+      </List>
     </Flex>
   );
 }
